319a241260f5060201ff8681c0f227a4e3be0bff
pavithpr89 <pavithpr89@gmail.com>
2025-12-15 08:11:00 +0000

    Did the 0

--- sort.h
+++ sort.h
@@ -3,7 +3,16 @@
 
 #include <stddef.h>
 
-/* Doubly linked list node structure */
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ *
+ * Description: Node structure used for sorting algorithms that
+ *              work on doubly linked lists.
+ */
 typedef struct listint_s
 {
 	const int n;
@@ -11,9 +20,27 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Function prototypes */
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ *               using the Bubble sort algorithm.
+ * @array: Pointer to the array to sort
+ * @size: Number of elements in the array
+ *
+ * Description: Prints the array each time two elements are swapped.
+ */
 void bubble_sort(int *array, size_t size);
+
+/**
+ * print_array - Prints an array of integers
+ * @array: Pointer to the array to print
+ * @size: Number of elements in the array
+ */
 void print_array(const int *array, size_t size);
+
+/**
+ * print_list - Prints a doubly linked list of integers
+ * @list: Pointer to the list to print
+ */
 void print_list(const listint_t *list);
 
 #endif /* SORT_H */
